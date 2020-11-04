d059bf951fd3be3afae3c111f86e1276d9c53469
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-04 16:15:49 +0700

    Add all file

---
+++ src/com/onequation/Main.java
@@ -0,0 +1,29 @@
+package com.onequation;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = 0, please enter constans: ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+        System.out.print("c: ");
+        double c = scanner.nextDouble();
+
+        if (a != 0) {
+            double solution = -b / a;
+            System.out.printf("The solution is: %f!", solution);
+        }else {
+            if (b == 0){
+                System.out.println("The solution is all x!");
+            }else {
+                System.out.println("No solution!");
+            }
+        }
+    }
+}
